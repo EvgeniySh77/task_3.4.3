acd29c291d859f38f3bad3aa68cc6c7c71563920
EvgeniySh77 <evgen.sh77@mail.ru>
2021-12-25 21:55:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task_3._4._3
+{
+    enum Semaphore
+    {
+        Red = 100,
+        Yellow = 200,
+        Green =300
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Semaphore.Red + "\n" + Semaphore.Yellow + "\n" + Semaphore.Green);
+
+            Console.ReadLine();
+        }
+    }
+}
